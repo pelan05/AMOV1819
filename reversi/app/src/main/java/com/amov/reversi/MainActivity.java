b08561af826cb7ae01d52322b66eeb3b5a78e4c0
Pedro Lanhoso <pelan05@gmail.com>
2018-12-24 02:08:35 +0000

    appcompat begone

--- reversi/app/src/main/java/com/amov/reversi/MainActivity.java
+++ reversi/app/src/main/java/com/amov/reversi/MainActivity.java
@@ -1,22 +1,19 @@
 package com.amov.reversi;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
-
 
 
 
@@ -78,8 +75,6 @@ public class MainActivity extends AppCompatActivity {
         intentClient.putExtra("mode", MainActivityGame.CLIENT);
         startActivity(intentClient);
     }
-
-
 
 
 
